41c4ac49dfe6e8e5bdc0f1372bde5f94c265ea42
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-14 04:40:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
+
 def fizzbuzz(number)
-  if number == % 3
+  if number % 3 == 0
     return "Fizz"
   end
   
 def fizzbuzz(number)
-  if number == 
+  if number % 5 == 0 
+    return "Buzz"
+  end
